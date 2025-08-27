a5737bebf0a3e02706c1f7123c5ffbe9f2472374
Ozeas01 <120319182+Ozeas01@users.noreply.github.com>
2025-08-27 14:17:58 -0300

    Update variables.tf

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -4,5 +4,5 @@ variable "plan_name"           { type = string }
 variable "plan_sku"            { type = string }
 variable "plan_os"             { type = string } # "Linux" ou "Windows"
 variable "app_name"            { type = string }
-variable "runtime_stack"       { type = string } # ex: NODE|18-lts
+variable "runtime_stack"       { type = string } 
 variable "app_settings"        { type = map(string) default = {} }

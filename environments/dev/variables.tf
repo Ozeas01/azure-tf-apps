9df9f0da728500bd8cd829777382542eb2b4fc20
Ozeas01 <120319182+Ozeas01@users.noreply.github.com>
2025-08-27 16:31:20 -0300

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -5,11 +5,11 @@ variable "tags"           { type = map(string) default = {} }
 variable "apps" {
   description = "Mapa de apps a serem criados neste ambiente"
   type = map(object({
-    plan_name     = string        # nome do App Service Plan
-    plan_sku      = string        # ex: B1, P1v3
-    plan_os       = string        # Linux ou Windows
-    app_name      = string        # nome do Web App
-    runtime_stack = string        # ex: NODE|18-lts, DOTNETCORE:8.0
+    plan_name     = string       
+    plan_sku      = string       
+    plan_os       = string       
+    app_name      = string       
+    runtime_stack = string       
     app_settings  = optional(map(string), {})
   }))
   default = {}

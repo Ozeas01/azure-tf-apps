fd1fe61c6caea2051bbae2895c10e80ca26bcfbd
Ozeas01 <120319182+Ozeas01@users.noreply.github.com>
2025-08-27 14:04:00 -0300

    Delete modules/app_service/main.tf directory

--- modules/app_service/main.tf/versions.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_version = ">= 1.6"
-  required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~> 3.113" }
-    random  = { source = "hashicorp/random",  version = "~> 3.6" }
-  }
-}

4df38d10f7e38f4c5602e0bb91e42db10e3c632b
Ozeas01 <120319182+Ozeas01@users.noreply.github.com>
2025-08-27 14:10:11 -0300

    Create variables.tf

---
+++ modules/app_service/variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "plan_name"           { type = string }
+variable "plan_sku"            { type = string }
+variable "plan_os"             { type = string } # "Linux" ou "Windows"
+variable "app_name"            { type = string }
+variable "runtime_stack"       { type = string } # ex: NODE|18-lts
+variable "app_settings"        { type = map(string) default = {} }

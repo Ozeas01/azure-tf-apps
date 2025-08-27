86b8b8cd3510901f652088828e8ec1418b92574f
Ozeas01 <120319182+Ozeas01@users.noreply.github.com>
2025-08-27 16:07:42 -0300

    Update variables.tf

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,3 +1,16 @@
 variable "location"       { type = string }
 variable "resource_group" { type = string }
 variable "tags"           { type = map(string) default = {} }
+
+variable "apps" {
+  description = "Mapa de apps a serem criados neste ambiente"
+  type = map(object({
+    plan_name     = string        # nome do App Service Plan
+    plan_sku      = string        # ex: B1, P1v3
+    plan_os       = string        # Linux ou Windows
+    app_name      = string        # nome do Web App
+    runtime_stack = string        # ex: NODE|18-lts, DOTNETCORE:8.0
+    app_settings  = optional(map(string), {})
+  }))
+  default = {}
+}
